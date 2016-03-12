7810415f2a8f6da794c530dbed6c622926c11b38
kingleekong <konglee873@gmail.com>
2016-03-12 12:08:26 +0800

    Initial Commit

---
+++ GaoDeMapDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GaoDeMapDemo
+//
+//  Created by kong on 16/3/12.
+//  Copyright © 2016年 kong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
